ab911726a70bcebd47293e228f326d6104f4d43c
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-01-06 14:28:30 -0300

    First commit

---
+++ public/controllers/template.controller.php
@@ -0,0 +1,11 @@
+<?php
+
+// namespace POSSytem;
+
+class ControllerTemplate
+{
+    public function ctrTemplate()
+    {
+        include "views/template.php";
+    }
+}
---
+++ public/models/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ */
+class Connection {
+
+  /**
+   *
+   */
+  public static function connect() {
+
+    $link = new PDO("mysql:host=localhost;dbname=pos-english", "root", "");
+
+    $link->exec("set names utf8");
+
+    return $link;
+  }
+
+}
